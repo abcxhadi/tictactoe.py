25fd17c730adaa40c328795eee9d7ecf6173b433
abcxhadi <abcxhadi@gmail.com>
2025-06-28 07:27:35 +0400

    try and except for errors

--- project.py
+++ project.py
@@ -3,17 +3,20 @@ game = [[0, 0, 0],
         [0, 0, 0]]
 
 
-def game_board(current_game, player=0, row=0, column=0, just_display=False):
-    print("   0  1  2")
-    if not just_display:
-        game[row][column] = player
-    for count, row in enumerate(current_game):
-        print(count, row)
-    return current_game
-
+def game_board(game_map, player=0, row=0, column=0, just_display=False):
+    try:
+        print("   0  1  2")
+        if not just_display:
+            game_map[row][column] = player
+        for count, row in enumerate(game_map):
+            print(count, row)
+        return game_map
+    except:
+        print("nah you cant do that")
+        return False
 
 game_board(game)
-game_board(game, player=2, row=0, column=0)
-
-print(game)
 print(id(game))
+game_board(game, player=2, row=10, column=0)
+print(id(game))
+
